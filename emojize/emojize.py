d84ac5583443e522b2bf634b4b03e4d7bc2b72be
Stealth <60522445+Arnav1771@users.noreply.github.com>
2024-08-16 20:14:23 +0000

    Sat, Aug 17, 2024, 1:44 AM +05:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,16 +1,5 @@
-# emojize.py
-
 import emoji
 
-def main():
-    # Prompt the user for a string
-    user_input = input("Input: ")
+input = input("Input: ")
 
-    # Convert emoji codes and aliases to their corresponding emoji characters
-    emojized_string = emoji.emojize(user_input, use_aliases=True)
-
-    # Print the emojized string
-    print("Output:", emojized_string)
-
-if __name__ == "__main__":
-    main()
+print("Output:", emoji.emojize(input))
